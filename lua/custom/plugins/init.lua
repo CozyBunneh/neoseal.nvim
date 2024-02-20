c6492b772345ed640e896163f7a817d45211777c
Julia Mattjus <jmattjus@apple.com>
2024-02-20 18:38:34 +0100

    add harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,6 +38,32 @@ return {
     end,
   },
 
+  -- Buffers
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+    end
+  },
+
   -- File Tree
   {
     "prichrd/netrw.nvim",
@@ -258,8 +284,35 @@ return {
         vim.keymap.set('n', '<leader>cs', function()
           require('telescope').extensions.metals.commands()
         end, { silent = true, buffer = bufnr, desc = 'Scala: Commands' })
+
+        -- LSP mappings
+        map("n", "gD", vim.lsp.buf.definition)
+        map("n", "K", vim.lsp.buf.hover)
+        map("n", "gi", vim.lsp.buf.implementation)
+        map("n", "gr", vim.lsp.buf.references)
+        map("n", "gds", vim.lsp.buf.document_symbol)
+        map("n", "gws", vim.lsp.buf.workspace_symbol)
+        map("n", "<leader>cl", vim.lsp.codelens.run)
+        map("n", "<leader>sh", vim.lsp.buf.signature_help)
+        map("n", "<leader>rn", vim.lsp.buf.rename)
+        map("n", "<leader>f", vim.lsp.buf.format)
+        map("n", "<leader>ca", vim.lsp.buf.code_action)
+
         map("n", "<leader>ws", function()
           require("metals").hover_worksheet()
+        end)
+
+        -- all workspace diagnostics
+        map("n", "<leader>aa", vim.diagnostic.setqflist)
+
+        -- all workspace errors
+        map("n", "<leader>ae", function()
+          vim.diagnostic.setqflist({ severity = "E" })
+        end)
+
+        -- all workspace warnings
+        map("n", "<leader>aw", function()
+          vim.diagnostic.setqflist({ severity = "W" })
         end)
       end
 
